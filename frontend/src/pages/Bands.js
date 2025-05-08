b33ca497cbd7868bcac29cfd0605e56b52a1dd44
idelija92 <idelija92@gmail.com>
2025-05-08 17:38:21 +0100

    Added basic styling to bands page

--- frontend/src/pages/Bands.js
+++ frontend/src/pages/Bands.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import API from '../services/api';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import "./Bands.css";
 
 const Bands = () => {
   const [bands, setBands] = useState([]);
+  const auth = useContext(AuthContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,14 +30,23 @@ const Bands = () => {
 
 
   return (
-    <div>
-      <h1>Bands</h1>
+<div className="bands-container">
+      <h1 className="bands-title">Bands</h1>
       <Link to="/">← Back to Home</Link>
-      <ul>
+
+      <ul className="bands-list">
         {bands.map(band => (
-          <li key={band.id}>{band.name} ({band.genre}) - {band.description}  {' '}
-            <Link to={`/bands/edit/${band.id}`}>[Edit]</Link> {' '}
-            <button onClick={() => handleDelete(band.id)}>Delete</button></li>
+          <li key={band.id} className="bands-item">
+            <div className="bands-name">{band.name}</div>
+            <div className="bands-meta">{band.genre}</div>
+            <div className="bands-meta">{band.description}</div>
+            {auth.role === 'ADMIN' && (
+              <div className="bands-actions">
+                <Link to={`/bands/edit/${band.id}`}>Edit</Link>
+                <button onClick={() => handleDelete(band.id)}>Delete</button>
+              </div>
+            )}
+          </li>
         ))}
       </ul>
     </div>
