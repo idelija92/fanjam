5be34ae22f41e0b41e371c5f67c637f3952ec808
idelija92 <idelija92@gmail.com>
2025-07-07 14:13:14 +0100

    Fix import link

--- frontend/src/pages/EventWinnersPage.js
+++ frontend/src/pages/EventWinnersPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import API from '../api/api';
+import API from '../services/api';
 
 const EventWinnersPage = () => {
     const { eventId } = useParams();
