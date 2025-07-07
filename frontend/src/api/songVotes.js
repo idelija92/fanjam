bf7c9ee50e60b1e466f6455c9854217166ba90c1
idelija92 <idelija92@gmail.com>
2025-07-07 13:52:59 +0100

    Fix hardcoded localhost in song-votes.js

--- frontend/src/api/songVotes.js
+++ frontend/src/api/songVotes.js
@@ -1,33 +1,21 @@
-import axios from 'axios';
-
-const API = 'http://localhost:8080/api/song-votes';
+import API from './api';
 
 export const voteForSong = async (eventId, songTitle, token, customMessage = '') => {
-    return axios.post(
-        API,
-        new URLSearchParams({ eventId, songTitle, customMessage }),
-        {
-            headers: { Authorization: `Bearer ${token}` }
-        }
-    );
+  return API.post(
+    '/song-votes',
+    new URLSearchParams({ eventId, songTitle, customMessage })
+  );
 };
 
-
 export const unvoteForSong = async (eventId, songTitle, token) => {
-    return axios.delete(
-        API,
-        {
-            headers: { Authorization: `Bearer ${token}` },
-            data: new URLSearchParams({ eventId, songTitle })
-        }
-    );
+  return API.delete(
+    '/song-votes',
+    {
+      data: new URLSearchParams({ eventId, songTitle })
+    }
+  );
 };
 
 export const getVotesForEvent = async (eventId, token) => {
-    return axios.get(
-        `${API}/event/${eventId}`,
-        {
-            headers: { Authorization: `Bearer ${token}` }
-        }
-    );
+  return API.get(`/song-votes/event/${eventId}`);
 };
