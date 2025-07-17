c04fedc2452aea1eb8a3f5707e681aacc27f5ddf
idelija92 <idelija92@gmail.com>
2025-07-17 09:34:29 +0100

    Fix links in events winner page - fix import

--- frontend/src/pages/EventWinnersPage.js
+++ frontend/src/pages/EventWinnersPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-import API from '../api';
+import API from '../services/api';
 import { Card, ListGroup, Badge, Spinner, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Nav } from 'react-bootstrap';
